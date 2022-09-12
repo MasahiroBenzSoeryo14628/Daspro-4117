ee8fa0995a3358462b33687fdc47f9079800d66d
MasahiroBenzSoeryo14628 <112880386+MasahiroBenzSoeryo14628@users.noreply.github.com>
2022-09-12 21:09:30 +0700

    Add files via upload

---
+++ Pertemuan3/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    int x = 2000;
+
+    cout << "Hello World! " << x << endl;
+
+    /* comment */
+
+    string nama;
+    cout << "\nTuliskan Nama: ";
+    cin >> nama;
+    cout << "Hi " << nama;
+    cout << " Selamat Datang!" << endl;
+
+    cout << "\nNama Saya Masahiro Benz Soeryo" << endl;
+
+    int u = 18;
+    float t = 174.5;
+    char c = 'L';
+    string b = "XL";
+
+    cout << "\nUmur: " << u;
+    cout << "\nTinggi: " << t;
+    cout << "\nUkuran baju: " << b;
+    cout << "\nUkuran celana: " << c;
+
+    return 0;
+}
